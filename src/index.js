022fed1e31a24c547dcf828cd53c1fc8b84f7aab
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-06-29 19:37:08 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,85 @@
+//const element = document.getElementById(Id)
+/* let topo = document.getElementById('cabecalho')
+console.log(topo)
+topo.style.border = 'solid 5px black' */
+
+
+//let elements = document.getElementByTagName(tagName)[]
+
+/* let titulo = document.getElementsByTagName('h2')[0]
+titulo.innerText = 'Agora meu texto é esse aqui'
+titulo.style.color = 'green'
+
+let sub = document.getElementsByTagName('h2')[1]
+console.log(sub)
+let btn = document.getElementsByTagName('button')[0]
+btn.innerText = "Mudança"
+btn.style.fontSize = '20px'
+
+let corpo = document.body
+corpo.style.backgroundColor = 'red'
+let image = document.getElementsByTagName('img')[0]
+
+function clicar(){
+    sub.innerText = 'Eu mudei'
+    sub.style.color = 'blue'
+    corpo.style.backgroundColor = 'black'
+    image.style.display = 'none'
+} */
+
+
+// let elements = documents.getElementsByClassName(names);
+
+/* let menu = document.getElementById('menu')
+let items = menu.getElementsByClassName('list')
+
+let data = [].map.call(items, item => item.textContent)
+ */
+/* console.log(data) */
+
+
+//querySelector  let element = parentNode.querySelector(seletor)
+
+//querySelectorAll let elements = parentNode.querySelectorAll(seletor)
+
+
+/* let nodeList = document.querySelectorAll('.list')
+console.log(nodeList)
+let elements = Array.from(nodeList)
+
+elements.pop()
+console.log(elements) */
+
+/* // 1 seletor universal *
+let element = document.querySelector('*')
+console.log(element)
+let elements = document.querySelectorAll('*')
+console.log(elements) */
+
+/* //2 seletor tipo
+let tituloPrincipal = document.querySelector('h1')
+tituloPrincipal.innerText = 'Olá Mundo!'
+
+let subTitulo = document.querySelectorAll('h2')
+console.log(subTitulo) */
+
+//3 seletor por id
+/* let cabecalho  = document.querySelector('#cabecalho')
+console.log(cabecalho) */
+
+//4 seletor por class
+
+/* let item = document.querySelector('.list')
+console.log(item)
+let items = document.querySelectorAll('.list')
+console.log(items) */
+
+//5  seletor de atributo
+//[atributo]
+//[atrituro~=valor]
+
+/* let  dado = document.querySelector('[alt]')
+console.log(dado)
+
+let  dados = document.querySelectorAll('[alt]')
+console.log(dados) */
